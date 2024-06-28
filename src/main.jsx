b3942f26aa54557c9c97a290da66e363e0c070f6
Yoshi Nagi <nagiyoshi13@gmail.com>
2024-06-28 10:24:46 +0900

    Add star rating component

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+// import App from './App.jsx'
+// import './index.css'
+
+import StarRating from './StarRating';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    {/* <App /> */}
+    <StarRating
+      maxRating={5}
+      messages={['terrible', 'bad', 'okay', 'good', 'excellent']}
+    />
+    <StarRating size={24} color="red" defaultRating={3} />
+  </React.StrictMode>
+);
