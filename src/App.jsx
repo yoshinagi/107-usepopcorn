32f813cc6ccebfd3f75690bb72c26519ae49c60b
Yoshi Nagi <nagiyoshi13@gmail.com>
2024-06-28 18:51:37 +0900

    Add movie fetching function

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/prop-types */
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 const tempMovieData = [
   {
@@ -51,9 +51,17 @@ const tempWatchedData = [
 const average = (arr) =>
   arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
 
+const KEY = 'f84c4b5e';
+
 export default function App() {
   const [movies, setMovies] = useState(tempMovieData);
   const [watched, setWatched] = useState(tempWatchedData);
+
+  useEffect(function () {
+    fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=interstellar`)
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  }, []);
 
   return (
     <>
--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import App from './App.jsx'
-// import './index.css'
+import App from './App.jsx';
+import './index.css';
 
-import StarRating from './StarRating';
+// import StarRating from './StarRating';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* <App /> */}
-    <StarRating
+    <App />
+    {/* <StarRating
       maxRating={5}
       messages={['terrible', 'bad', 'okay', 'good', 'excellent']}
     />
-    <StarRating size={24} color="red" defaultRating={3} />
+    <StarRating size={24} color="red" defaultRating={3} /> */}
   </React.StrictMode>
 );
